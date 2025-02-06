385326c2435f5b35d8e1cc769e5f13b0aa76dc45
Thevikashroy <vikashkrdss19@gmail.com>
2025-02-06 13:57:08 +0530

    Initialize React app with Vite and Tailwind CSS setup

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card'
+import Card from './components/cNew-Item -ItemType File -Name {file_name}ard'
 
 function App() {
   const [count, setCount] = useState(0)
   let myObj = {
-    username: "Vikash",
+    username: "hitesh",
     age: 21
   }
   let newArr = [1, 2, 3]
@@ -16,7 +16,7 @@ function App() {
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
       <Card username="chaiaurcode" btnText="click me" />
-      <Card username="Vikash" btnText='visit me'/>
+      <Card username="hitesh" />
     </>
   )
 }
